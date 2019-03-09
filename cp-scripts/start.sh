1a7e6a2aa825dd4708e10376e172acfd18f03529
雪あすか <tt@kmycode.net>
2019-03-09 23:12:25 +0900

    #15 武将画像アップロード用に、ディレクトリ退避・移動をスクリプトに組み込み

--- cp-scripts/start.sh
+++ cp-scripts/start.sh
@@ -4,6 +4,8 @@ cp /home/sangokukmy/publish_settings/appsettings.Production.json /home/sangokukm
 pwd +r /home/sangokukmy/publish/appsettings.Production.json
 mkdir -p /home/sangokukmy/publish/wwwroot/images/character-default-icons
 cp /home/sangokukmy/publish_settings/default-images/*.gif /home/sangokukmy/publish/wwwroot/images/character-default-icons
+mv /home/sangokukmy/publish_settings/character-uploaded-icons /home/sangokukmy/publish/wwwroot/images
 mkdir /home/sangokukmy/publish/logs
 chmod 0777 /home/sangokukmy/publish/logs
+chmod 0777 /home/sangokukmy/publish/wwwroot/images/character-uploaded-icons
 systemctl start sangokukmy
--- cp-scripts/stop.sh
+++ cp-scripts/stop.sh
@@ -1,5 +1,6 @@
 ﻿#!/bin/bash
 
 systemctl stop sangokukmy
+mv /home/sangokukmy/publish/wwwroot/images/character-uploaded-icons /home/sangokukmy/publish_settings
 rm -rf /home/sangokukmy/publish
 mkdir /home/sangokukmy/publish
