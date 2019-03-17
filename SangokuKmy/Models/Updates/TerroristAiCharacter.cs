85d05a2c59518a6d7a5e8754ebc280d65b6e6da5
雪あすか <tt@kmycode.net>
2019-03-17 17:41:58 +0900

    前回のコミットに追加

--- SangokuKmy/Models/Updates/TerroristAiCharacter.cs
+++ SangokuKmy/Models/Updates/TerroristAiCharacter.cs
@@ -63,6 +63,8 @@ namespace SangokuKmy.Models.Updates
 
   public class TerroristCivilOfficialAiCharacter : FarmerCivilOfficialAiCharacter
   {
+    protected override bool CanSoldierForce => false;
+
     public TerroristCivilOfficialAiCharacter(Character character) : base(character)
     {
     }
