5e7ab38debadfae33deac342741018e51f28fd06
雪あすか <tt@kmycode.net>
2019-03-30 18:52:28 +0900

    異民族AIの作戦リセット用に追加されたデータをEFに登録

---
+++ SangokuKmy/Migrations/20190330095201_AddWorkerAiResetTime.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SangokuKmy.Migrations
+{
+    public partial class AddWorkerAiResetTime : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "next_reset_game_date",
+                table: "ai_country_storategies",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "next_reset_game_date",
+                table: "ai_country_storategies");
+        }
+    }
+}
