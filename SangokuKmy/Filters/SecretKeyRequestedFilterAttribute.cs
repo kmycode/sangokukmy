d6db260786f878f3f23247515a84ed7c1b0c6887
雪あすか <tt@kmycode.net>
2019-03-30 13:21:27 +0900

    サーバの一部APIにシークレットキー制限を追加

---
+++ SangokuKmy/Filters/SecretKeyRequestedFilterAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.Filters;
+using SangokuKmy.Models.Common;
+using SangokuKmy.Models.Common.Definitions;
+
+namespace SangokuKmy.Filters
+{
+  public class SecretKeyRequestedFilterAttribute : ActionFilterAttribute
+  {
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+      base.OnActionExecuting(context);
+
+      string key = context.HttpContext.Request.Headers["SecretKey"];
+      if (!string.IsNullOrEmpty(Config.Game.SecretKey))
+      {
+        if (key != Config.Game.SecretKey)
+        {
+          ErrorCode.InvalidSecretKeyError.Throw();
+        }
+      }
+    }
+  }
+}
