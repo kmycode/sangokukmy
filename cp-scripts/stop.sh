9f94efef7e74040db169cfb75615e42a91b712e5
雪あすか <tt@kmycode.net>
2019-03-30 13:23:22 +0900

    デプロイ時のシェルスクリプトから不要な処理を削除

--- cp-scripts/stop.sh
+++ cp-scripts/stop.sh
@@ -1,7 +1,5 @@
 ﻿#!/bin/bash
 
 systemctl stop sangokukmy
-mv /home/sangokukmy/publish/wwwroot/images/character-uploaded-icons /home/sangokukmy/publish_settings
-mv /home/sangokukmy/publish/wwwroot/images/character-historical-uploaded-icons /home/sangokukmy/publish_settings
 rm -rf /home/sangokukmy/publish
 mkdir /home/sangokukmy/publish
