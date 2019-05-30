20b40d20a2d3d24f16c8eda28d0428682e84b72d
雪あすか <tt@kmycode.net>
2019-05-31 07:05:27 +0900

    リセット時、マネージド国家を作らないよう修正

--- SangokuKmy/Models/Services/ResetService.cs
+++ SangokuKmy/Models/Services/ResetService.cs
@@ -80,18 +80,6 @@ namespace SangokuKmy.Models.Services
       });
       await repo.SaveChangesAsync();
 
-      Func<EventType, string, bool, Task> maplog = async (type, message, isImportant) => await repo.MapLog.AddAsync(new MapLog
-      {
-        Message = message,
-        EventType = type,
-        IsImportant = isImportant,
-        Date = DateTime.Now,
-        ApiGameDateTime = system.GameDateTime,
-      });
-      await AiService.CreateManagedCountryAsync(repo, maplog, 2);
-      await AiService.CreateManagedCountryAsync(repo, maplog, 0);
-      await repo.SaveChangesAsync();
-
       await StatusStreaming.Default.SendAllAsync(ApiData.From(new ApiSignal
       {
         Type = SignalType.Reseted,
