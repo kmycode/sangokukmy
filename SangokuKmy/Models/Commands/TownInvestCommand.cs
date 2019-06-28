19ad5b8bc0c6d05b7cca2389e1565a6c6ec49774
雪あすか <tt@kmycode.net>
2019-06-29 08:36:24 +0900

    都市投資で入手するアイテムを保留状態に切り替え

--- SangokuKmy/Models/Commands/TownInvestCommand.cs
+++ SangokuKmy/Models/Commands/TownInvestCommand.cs
@@ -35,7 +35,7 @@ namespace SangokuKmy.Models.Commands
               var info = CharacterItemInfoes.Get(item.Type);
               if (info.HasData)
               {
-                await ItemService.SetCharacterAsync(repo, item, chara.Data);
+                await ItemService.SetCharacterPendingAsync(repo, item, chara.Data);
                 results.Add($"アイテム {info.Data.Name}");
               }
             }
