148cd20b5a7441a90ba036ab591ade3f6fc92891
雪あすか <tt@kmycode.net>
2019-08-04 09:22:17 +0900

    戦闘ログに武将の出身を追加

---
+++ SangokuKmy/Migrations/20190804002148_AddBattleLogCharacterFrom.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SangokuKmy.Migrations
+{
+    public partial class AddBattleLogCharacterFrom : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "from",
+                table: "character_caches",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "from",
+                table: "character_caches");
+        }
+    }
+}
