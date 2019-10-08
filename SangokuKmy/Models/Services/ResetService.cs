af7fb253d67aa101c63053d3bd699f7452835324
雪あすか <tt@kmycode.net>
2019-10-08 20:04:17 +0900

    都市数２追加

--- SangokuKmy/Models/Services/ResetService.cs
+++ SangokuKmy/Models/Services/ResetService.cs
@@ -221,7 +221,7 @@ namespace SangokuKmy.Models.Services
 
     private static async Task ResetTownsAndSaveAsync(MainRepository repo)
     {
-      var initialTowns = MapService.CreateMap(RandomService.Next(8, 11));
+      var initialTowns = MapService.CreateMap(RandomService.Next(10, 12));
       var towns = new List<Town>();
       foreach (var itown in initialTowns)
       {
