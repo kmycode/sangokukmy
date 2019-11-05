dd0534d932c91bdacc22e4dbddbc6324e189e98c
雪あすか <tt@kmycode.net>
2019-11-05 10:22:48 +0900

    部隊、外交関係のAPIを別コントローラに切り分け

---
+++ SangokuKmy/Controllers/DiplomacyController.cs
@@ -0,0 +1,401 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SangokuKmy.Common;
+using SangokuKmy.Filters;
+using SangokuKmy.Models.Common.Definitions;
+using SangokuKmy.Models.Data;
+using SangokuKmy.Models.Data.ApiEntities;
+using SangokuKmy.Models.Data.Entities;
+using SangokuKmy.Models.Services;
+using SangokuKmy.Streamings;
+
+namespace SangokuKmy.Controllers
+{
+  [Route("api/v1")]
+  [ServiceFilter(typeof(SangokuKmyErrorFilterAttribute))]
+  public class DiplomacyController : Controller, IAuthenticationDataReceiver
+  {
+    private readonly ILogger _logger;
+    public AuthenticationData AuthData { private get; set; }
+
+    public DiplomacyController(ILogger<SangokuKmyController> logger)
+    {
+      this._logger = logger;
+    }
+
+    [AuthenticationFilter]
+    [HttpPut("country/{targetId}/alliance")]
+    public async Task SetCountryAllianceAsync(
+      [FromRoute] uint targetId,
+      [FromBody] CountryAlliance param)
+    {
+      CountryAlliance alliance;
+      MapLog mapLog = null;
+
+      if (param.Status != CountryAllianceStatus.Available &&
+          param.Status != CountryAllianceStatus.ChangeRequesting &&
+          param.Status != CountryAllianceStatus.Dismissed &&
+          param.Status != CountryAllianceStatus.InBreaking &&
+          param.Status != CountryAllianceStatus.Requesting &&
+          param.Status != CountryAllianceStatus.None)
+      {
+        ErrorCode.InvalidParameterError.Throw();
+      }
+
+      using (var repo = MainRepository.WithReadAndWrite())
+      {
+        var self = await repo.Character.GetByIdAsync(this.AuthData.CharacterId).GetOrErrorAsync(ErrorCode.LoginCharacterNotFoundError);
+        var posts = await repo.Country.GetPostsAsync(self.CountryId);
+        var myPost = posts.FirstOrDefault(p => p.CharacterId == self.Id);
+        if (myPost == null || !myPost.Type.CanDiplomacy())
+        {
+          ErrorCode.NotPermissionError.Throw();
+        }
+
+        var target = await repo.Country.GetAliveByIdAsync(targetId).GetOrErrorAsync(ErrorCode.CountryNotFoundError);
+
+        var old = await repo.CountryDiplomacies.GetCountryAllianceAsync(self.CountryId, targetId);
+        var war = await repo.CountryDiplomacies.GetCountryWarAsync(self.CountryId, targetId);
+        if (old.HasData &&
+          old.Data.Status != CountryAllianceStatus.Broken &&
+          old.Data.Status != CountryAllianceStatus.Dismissed &&
+          old.Data.Status != CountryAllianceStatus.None)
+        {
+          var o = old.Data;
+
+          if ((param.Status == CountryAllianceStatus.Available || param.Status == CountryAllianceStatus.ChangeRequesting) &&
+                o.Status == CountryAllianceStatus.Requesting)
+          {
+            if (self.CountryId == o.RequestedCountryId)
+            {
+              // 自分で自分の要求を承認しようとした
+              ErrorCode.NotPermissionError.Throw();
+            }
+          }
+          else if (param.Status == CountryAllianceStatus.InBreaking &&
+                    (o.Status != CountryAllianceStatus.Available && o.Status != CountryAllianceStatus.ChangeRequesting))
+          {
+            // 結んでいないのに破棄はエラー
+            ErrorCode.NotPermissionError.Throw();
+          }
+          else if ((param.Status == CountryAllianceStatus.None || param.Status == CountryAllianceStatus.Requesting) &&
+                    (o.Status == CountryAllianceStatus.Available || o.Status == CountryAllianceStatus.ChangeRequesting))
+          {
+            // 結んでいるものを一瞬でなかったことにするのはエラー
+            ErrorCode.NotPermissionError.Throw();
+          }
+          else if (param.Status == o.Status && param.Status == CountryAllianceStatus.Available)
+          {
+            // 再承認はできない
+            ErrorCode.MeaninglessOperationError.Throw();
+          }
+
+          if (param.Status == CountryAllianceStatus.Available)
+          {
+            param.BreakingDelay = o.BreakingDelay;
+            param.IsPublic = o.IsPublic;
+          }
+        }
+        else
+        {
+          if (param.Status != CountryAllianceStatus.Requesting)
+          {
+            // 同盟を結んでいない場合、同盟の要求以外にできることはない
+            ErrorCode.NotPermissionError.Throw();
+          }
+
+          war.Some((w) =>
+          {
+            if (w.Status == CountryWarStatus.Available ||
+                w.Status == CountryWarStatus.InReady ||
+                w.Status == CountryWarStatus.StopRequesting)
+            {
+              // 戦争中
+              ErrorCode.NotPermissionError.Throw();
+            }
+          });
+        }
+
+        alliance = new CountryAlliance
+        {
+          RequestedCountryId = self.CountryId,
+          InsistedCountryId = targetId,
+          BreakingDelay = param.BreakingDelay,
+          IsPublic = param.IsPublic,
+          Status = param.Status,
+          NewBreakingDelay = param.NewBreakingDelay,
+        };
+        await repo.CountryDiplomacies.SetAllianceAsync(alliance);
+
+        // 同盟関係を周りに通知
+        if (alliance.IsPublic && old.HasData)
+        {
+          if (old.Data.Status == CountryAllianceStatus.Requesting &&
+              alliance.Status == CountryAllianceStatus.Available)
+          {
+            var country1 = await repo.Country.GetByIdAsync(alliance.RequestedCountryId).GetOrErrorAsync(ErrorCode.CountryNotFoundError);
+            var country2 = await repo.Country.GetByIdAsync(alliance.InsistedCountryId).GetOrErrorAsync(ErrorCode.CountryNotFoundError);
+            mapLog = new MapLog
+            {
+              ApiGameDateTime = (await repo.System.GetAsync()).GameDateTime,
+              Date = DateTime.Now,
+              EventType = EventType.AllianceStart,
+              IsImportant = true,
+              Message = "<country>" + country1.Name + "</country> と <country>" + country2.Name + "</country> は、同盟を締結しました",
+            };
+            await repo.MapLog.AddAsync(mapLog);
+          }
+        }
+
+        await repo.SaveChangesAsync();
+      }
+
+      // 同盟関係を周りに通知
+      if (alliance.IsPublic)
+      {
+        await StatusStreaming.Default.SendAllAsync(ApiData.From(alliance));
+        if (mapLog != null)
+        {
+          await StatusStreaming.Default.SendAllAsync(ApiData.From(mapLog));
+          await AnonymousStreaming.Default.SendAllAsync(ApiData.From(mapLog));
+        }
+      }
+      else
+      {
+        await StatusStreaming.Default.SendCountryAsync(ApiData.From(alliance), alliance.RequestedCountryId);
+        await StatusStreaming.Default.SendCountryAsync(ApiData.From(alliance), alliance.InsistedCountryId);
+      }
+    }
+
+    [AuthenticationFilter]
+    [HttpPut("country/{targetId}/war")]
+    public async Task SetCountryWarAsync(
+      [FromRoute] uint targetId,
+      [FromBody] CountryWar param)
+    {
+      CountryWar war;
+      Optional<CountryAlliance> alliance;
+      MapLog mapLog = null;
+
+      if (param.Status != CountryWarStatus.InReady /* &&
+          param.Status != CountryWarStatus.StopRequesting &&
+          param.Status != CountryWarStatus.Stoped */ )
+      {
+        ErrorCode.InvalidParameterError.Throw();
+      }
+
+      using (var repo = MainRepository.WithReadAndWrite())
+      {
+        var system = await repo.System.GetAsync();
+        var self = await repo.Character.GetByIdAsync(this.AuthData.CharacterId).GetOrErrorAsync(ErrorCode.LoginCharacterNotFoundError);
+        var posts = await repo.Country.GetPostsAsync(self.CountryId);
+        var myPost = posts.FirstOrDefault(p => p.CharacterId == self.Id);
+        if (myPost == null || !myPost.Type.CanDiplomacy())
+        {
+          ErrorCode.NotPermissionError.Throw();
+        }
+
+        var towns = await repo.Town.GetAllAsync();
+        var target = await repo.Country.GetAliveByIdAsync(targetId).GetOrErrorAsync(ErrorCode.CountryNotFoundError);
+
+        var old = await repo.CountryDiplomacies.GetCountryWarAsync(self.CountryId, targetId);
+        alliance = await repo.CountryDiplomacies.GetCountryAllianceAsync(self.CountryId, targetId);
+        old.Some((o) =>
+        {
+          if ((o.Status == CountryWarStatus.InReady || o.Status == CountryWarStatus.Available) &&
+              param.Status == CountryWarStatus.InReady)
+          {
+            // 重複して宣戦布告はできない
+            ErrorCode.MeaninglessOperationError.Throw();
+          }
+          else if (o.Status == CountryWarStatus.StopRequesting && param.Status == CountryWarStatus.Stoped &&
+                   o.RequestedCountryId == self.CountryId)
+          {
+            // 自分の停戦要求を自分で承認できない
+            ErrorCode.NotPermissionError.Throw();
+          }
+          else if (o.Status == CountryWarStatus.Stoped && param.Status == CountryWarStatus.StopRequesting)
+          {
+            // 一度決まった停戦を撤回できない
+            ErrorCode.NotPermissionError.Throw();
+          }
+
+          if (o.Status == CountryWarStatus.Stoped && param.Status == CountryWarStatus.InReady)
+          {
+            if (param.StartGameDate.ToInt() < system.IntGameDateTime + 12 * 12 + 1)
+            {
+              // 開戦が早すぎる
+              ErrorCode.InvalidParameterError.Throw();
+            }
+            else if (param.StartGameDate.ToInt() > system.IntGameDateTime + 12 * 24)
+            {
+              // 開戦が遅すぎる
+              ErrorCode.InvalidParameterError.Throw();
+            }
+          }
+          else
+          {
+            param.RequestedCountryId = o.RequestedCountryId;
+            param.InsistedCountryId = o.InsistedCountryId;
+            param.StartGameDate = o.StartGameDate;
+          }
+        });
+        old.None(() =>
+        {
+          if (!towns.GetAroundCountries(towns.Where(t => t.CountryId == param.InsistedCountryId)).Contains(param.RequestedCountryId))
+          {
+            // 飛び地布告
+            ErrorCode.InvalidOperationError.Throw();
+          }
+
+          if (param.Status == CountryWarStatus.StopRequesting || param.Status == CountryWarStatus.Stoped)
+          {
+            // 存在しない戦争を停戦にはできない
+            ErrorCode.NotPermissionError.Throw();
+          }
+          else if (param.StartGameDate.ToInt() < system.IntGameDateTime + 12 * 12 + 1)
+          {
+            // 開戦が早すぎる
+            ErrorCode.InvalidParameterError.Throw();
+          }
+          else if (param.StartGameDate.ToInt() > system.IntGameDateTime + 12 * 24)
+          {
+            // 開戦が遅すぎる
+            ErrorCode.InvalidParameterError.Throw();
+          }
+
+          alliance.Some((a) =>
+          {
+            if (a.Status == CountryAllianceStatus.Available ||
+                a.Status == CountryAllianceStatus.ChangeRequesting ||
+                a.Status == CountryAllianceStatus.InBreaking)
+            {
+              // 同盟が有効中
+              ErrorCode.NotPermissionError.Throw();
+            }
+            if (a.Status == CountryAllianceStatus.Requesting)
+            {
+              // 自動で同盟申請を却下する
+              a.Status = CountryAllianceStatus.Broken;
+            }
+          });
+        });
+
+        war = new CountryWar
+        {
+          RequestedCountryId = param.RequestedCountryId,
+          InsistedCountryId = param.InsistedCountryId,
+          StartGameDate = param.StartGameDate,
+          Status = param.Status,
+          RequestedStopCountryId = param.RequestedStopCountryId,
+        };
+
+        await CountryService.SendWarAndSaveAsync(repo, war);
+      }
+
+      if (alliance.HasData)
+      {
+        var a = alliance.Data;
+        await StatusStreaming.Default.SendCountryAsync(ApiData.From(a), a.RequestedCountryId);
+        await StatusStreaming.Default.SendCountryAsync(ApiData.From(a), a.InsistedCountryId);
+      }
+    }
+
+    [AuthenticationFilter]
+    [HttpPut("town/{townId}/war")]
+    public async Task SetTownWarAsync(
+      [FromRoute] uint townId)
+    {
+      using (var repo = MainRepository.WithReadAndWrite())
+      {
+        var system = await repo.System.GetAsync();
+        var chara = await repo.Character.GetByIdAsync(this.AuthData.CharacterId).GetOrErrorAsync(ErrorCode.LoginCharacterNotFoundError);
+        var posts = await repo.Country.GetPostsAsync(chara.CountryId);
+        var myPost = posts.FirstOrDefault(p => p.CharacterId == chara.Id);
+        if (myPost == null || !myPost.Type.CanDiplomacy())
+        {
+          ErrorCode.NotPermissionError.Throw();
+        }
+
+        var country = await repo.Country.GetAliveByIdAsync(chara.CountryId).GetOrErrorAsync(ErrorCode.CountryNotFoundError);
+        var targetTown = await repo.Town.GetByIdAsync(townId).GetOrErrorAsync(ErrorCode.TownNotFoundError);
+        var targetCountry = await repo.Country.GetAliveByIdAsync(targetTown.CountryId).GetOrErrorAsync(ErrorCode.CountryNotFoundError);
+
+        if (targetTown.Id == targetCountry.CapitalTownId)
+        {
+          // 首都への攻略
+          ErrorCode.InvalidOperationError.Throw();
+        }
+        if (await repo.Town.CountByCountryIdAsync(targetCountry.Id) <= 1)
+        {
+          // 残り１都市の国への攻略
+          ErrorCode.NotPermissionError.Throw();
+        }
+
+        var alliance = await repo.CountryDiplomacies.GetCountryAllianceAsync(country.Id, targetCountry.Id);
+        var countryWar = await repo.CountryDiplomacies.GetCountryWarAsync(country.Id, targetCountry.Id);
+        if (alliance.HasData)
+        {
+          if (alliance.Data.Status == CountryAllianceStatus.Available ||
+              alliance.Data.Status == CountryAllianceStatus.InBreaking ||
+              alliance.Data.Status == CountryAllianceStatus.ChangeRequesting ||
+              alliance.Data.Status == CountryAllianceStatus.Requesting)
+          {
+            ErrorCode.NotPermissionError.Throw();
+          }
+        }
+        if (countryWar.HasData)
+        {
+          if (countryWar.Data.Status == CountryWarStatus.Available ||
+              countryWar.Data.Status == CountryWarStatus.StopRequesting)
+          {
+            ErrorCode.MeaninglessOperationError.Throw();
+          }
+          if (countryWar.Data.Status == CountryWarStatus.InReady)
+          {
+            if (system.IntGameDateTime > countryWar.Data.IntStartGameDate - 6)
+            {
+              ErrorCode.NotPermissionError.Throw();
+            }
+          }
+        }
+        else
+        {
+          // 他国同士の戦争には介入できない
+          var wars = (await repo.CountryDiplomacies.GetAllWarsAsync())
+            .Where(w => w.RequestedCountryId == targetCountry.Id || w.InsistedCountryId == targetCountry.Id);
+          if (wars.Any(w => w.Status == CountryWarStatus.InReady || w.Status == CountryWarStatus.Available || w.Status == CountryWarStatus.StopRequesting))
+          {
+            ErrorCode.NotPermissionError.Throw();
+          }
+        }
+
+        var olds = (await repo.CountryDiplomacies.GetAllTownWarsAsync())
+          .Where(o => o.RequestedCountryId == country.Id);
+        if (olds.Any(o => o.Status == TownWarStatus.Available || o.Status == TownWarStatus.InReady))
+        {
+          // 重複して宣戦布告はできない
+          ErrorCode.MeaninglessOperationError.Throw();
+        }
+        else if (olds.Any(o => o.Status == TownWarStatus.Terminated &&
+                               system.IntGameDateTime - o.IntGameDate < 12 * 10))
+        {
+          ErrorCode.NotPermissionError.Throw();
+        }
+
+        var war = new TownWar
+        {
+          RequestedCountryId = country.Id,
+          InsistedCountryId = targetCountry.Id,
+          IntGameDate = system.IntGameDateTime + 1,
+          TownId = townId,
+          Status = TownWarStatus.InReady,
+        };
+        await CountryService.SendTownWarAndSaveAsync(repo, war);
+      }
+    }
+  }
+}
---
+++ SangokuKmy/Controllers/UnitController.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SangokuKmy.Common;
+using SangokuKmy.Filters;
+using SangokuKmy.Models.Common.Definitions;
+using SangokuKmy.Models.Data;
+using SangokuKmy.Models.Data.ApiEntities;
+using SangokuKmy.Models.Data.Entities;
+using SangokuKmy.Models.Services;
+using SangokuKmy.Streamings;
+
+namespace SangokuKmy.Controllers
+{
+  [Route("api/v1")]
+  [ServiceFilter(typeof(SangokuKmyErrorFilterAttribute))]
+  public class UnitController : Controller, IAuthenticationDataReceiver
+  {
+    private readonly ILogger _logger;
+    public AuthenticationData AuthData { private get; set; }
+
+    public UnitController(ILogger<SangokuKmyController> logger)
+    {
+      this._logger = logger;
+    }
+
+    [AuthenticationFilter]
+    [HttpGet("units")]
+    public async Task<IEnumerable<Unit>> GetCountryUnitsAsync()
+    {
+      using (var repo = MainRepository.WithRead())
+      {
+        var character = await repo.Character.GetByIdAsync(this.AuthData.CharacterId).GetOrErrorAsync(ErrorCode.CharacterNotFoundError);
+        var country = await repo.Country.GetAliveByIdAsync(character.CountryId).GetOrErrorAsync(ErrorCode.CountryNotFoundError);
+        return await repo.Unit.GetByCountryIdAsync(country.Id);
+      }
+    }
+
+    [AuthenticationFilter]
+    [HttpGet("units/{id}")]
+    public async Task<Unit> GetUnitAsync(
+      [FromRoute] uint id = default)
+    {
+      using (var repo = MainRepository.WithRead())
+      {
+        var character = await repo.Character.GetByIdAsync(this.AuthData.CharacterId).GetOrErrorAsync(ErrorCode.CharacterNotFoundError);
+        var unit = await repo.Unit.GetByIdAsync(id).GetOrErrorAsync(ErrorCode.UnitNotFoundError);
+        if (unit.CountryId != character.CountryId)
+        {
+          ErrorCode.NotPermissionError.Throw();
+        }
+        return unit;
+      }
+    }
+
+    [AuthenticationFilter]
+    [HttpPost("unit")]
+    public async Task CreateUnitAsync(
+      [FromBody] Unit param)
+    {
+      if (param == null)
+      {
+        ErrorCode.LackOfParameterError.Throw();
+      }
+
+      if (string.IsNullOrEmpty(param.Name) || param.Name.Length > 24)
+      {
+        ErrorCode.NumberRangeError.Throw(new ErrorCode.RangeErrorParameter("name", param.Name.Length, 1, 24));
+      }
+
+      if (param.Message?.Length > 240)
+      {
+        ErrorCode.NumberRangeError.Throw(new ErrorCode.RangeErrorParameter("message", param.Message.Length, 1, 240));
+      }
+
+      var unit = new Unit
+      {
+        Name = param.Name,
+        IsLimited = false,
+        Message = param.Message,
+      };
+      if (string.IsNullOrEmpty(unit.Name))
+      {
+        ErrorCode.LackOfNameParameterError.Throw();
+      }
+
+      using (var repo = MainRepository.WithReadAndWrite())
+      {
+        var character = await repo.Character.GetByIdAsync(this.AuthData.CharacterId).GetOrErrorAsync(ErrorCode.CharacterNotFoundError);
+        var country = await repo.Country.GetAliveByIdAsync(character.CountryId).GetOrErrorAsync(ErrorCode.CountryNotFoundError);
+
+        var old = await repo.Unit.GetByMemberIdAsync(character.Id);
+        if (old.Member.HasData)
+        {
+          var oldMember = old.Member.Data;
+          if (oldMember.Post == UnitMemberPostType.Leader)
+          {
+            // 隊長は２つ以上の部隊を持てない
+            ErrorCode.InvalidOperationError.Throw();
+          }
+        }
+
+        unit.CountryId = character.CountryId;
+        await repo.Unit.AddAsync(unit);
+        await repo.SaveChangesAsync();
+
+        await repo.Unit.SetMemberAsync(new UnitMember
+        {
+          CharacterId = character.Id,
+          Post = UnitMemberPostType.Leader,
+          UnitId = unit.Id,
+        });
+        await repo.SaveChangesAsync();
+      }
+    }
+
+    [AuthenticationFilter]
+    [HttpPost("unit/{id}/join")]
+    public async Task SetUnitMemberAsync(
+      [FromRoute] uint id)
+    {
+      using (var repo = MainRepository.WithReadAndWrite())
+      {
+        var character = await repo.Character.GetByIdAsync(this.AuthData.CharacterId).GetOrErrorAsync(ErrorCode.CharacterNotFoundError);
+        var country = await repo.Country.GetAliveByIdAsync(character.CountryId).GetOrErrorAsync(ErrorCode.CountryNotFoundError);
+        var unit = await repo.Unit.GetByIdAsync(id).GetOrErrorAsync(ErrorCode.UnitNotFoundError);
+
+        if (unit.CountryId != character.CountryId)
+        {
+          // 違う国の部隊には入れない
+          ErrorCode.NotPermissionError.Throw();
+        }
+        if (unit.IsLimited)
+        {
+          // 入隊制限がかかっている
+          ErrorCode.UnitJoinLimitedError.Throw();
+        }
+
+        var old = await repo.Unit.GetByMemberIdAsync(character.Id);
+        if (old.Member.HasData)
+        {
+          var oldMember = old.Member.Data;
+          if (oldMember.Post == UnitMemberPostType.Leader)
+          {
+            // 隊長は部隊から脱げられない
+            ErrorCode.InvalidOperationError.Throw();
+          }
+          if (oldMember.UnitId == unit.Id)
+          {
+            ErrorCode.MeaninglessOperationError.Throw();
+          }
+        }
+
+        await repo.Unit.SetMemberAsync(new UnitMember
+        {
+          CharacterId = character.Id,
+          Post = UnitMemberPostType.Normal,
+          UnitId = unit.Id,
+        });
+        await repo.SaveChangesAsync();
+      }
+    }
+
+    [AuthenticationFilter]
+    [HttpPost("unit/leave")]
+    public async Task LeaveUnitMemberAsync()
+    {
+      using (var repo = MainRepository.WithReadAndWrite())
+      {
+        var old = await repo.Unit.GetByMemberIdAsync(this.AuthData.CharacterId);
+        if (old.Member.HasData)
+        {
+          var oldMember = old.Member.Data;
+          if (oldMember.Post == UnitMemberPostType.Leader)
+          {
+            // 隊長は部隊から脱げられない
+            ErrorCode.InvalidOperationError.Throw();
+          }
+        }
+        else
+        {
+          ErrorCode.UnitNotFoundError.Throw();
+        }
+
+        UnitService.Leave(repo, this.AuthData.CharacterId);
+        await repo.SaveChangesAsync();
+      }
+    }
+
+    [AuthenticationFilter]
+    [HttpPut("unit/{id}")]
+    public async Task UpdateUnitAsync(
+      [FromRoute] uint id,
+      [FromBody] Unit unit)
+    {
+      if (unit == null)
+      {
+        ErrorCode.LackOfParameterError.Throw();
+      }
+
+      if (string.IsNullOrEmpty(unit.Name) || unit.Name.Length > 24)
+      {
+        ErrorCode.NumberRangeError.Throw(new ErrorCode.RangeErrorParameter("name", unit.Name.Length, 1, 24));
+      }
+
+      if (unit.Message?.Length > 240)
+      {
+        ErrorCode.NumberRangeError.Throw(new ErrorCode.RangeErrorParameter("message", unit.Message.Length, 1, 240));
+      }
+
+      using (var repo = MainRepository.WithReadAndWrite())
+      {
+        var member = await repo.Unit.GetByMemberIdAsync(this.AuthData.CharacterId);
+        var oldUnit = member.Unit.Data;
+        if (member.Unit.HasData && member.Member.HasData)
+        {
+          var unitMember = member.Member.Data;
+          if (unitMember.Post != UnitMemberPostType.Leader)
+          {
+            ErrorCode.NotPermissionError.Throw();
+          }
+          if (oldUnit.Id != id)
+          {
+            ErrorCode.NotPermissionError.Throw();
+          }
+        }
+        else
+        {
+          ErrorCode.UnitNotFoundError.Throw();
+        }
+
+        oldUnit.Message = unit.Message;
+        oldUnit.Name = unit.Name;
+        oldUnit.IsLimited = unit.IsLimited;
+        await repo.SaveChangesAsync();
+      }
+    }
+
+    [AuthenticationFilter]
+    [HttpPut("unit/{id}/leader")]
+    public async Task ChangeUnitLeaderAsync(
+      [FromRoute] uint id,
+      [FromBody] UnitMember newLeader)
+    {
+      if (newLeader == null)
+      {
+        ErrorCode.LackOfParameterError.Throw();
+      }
+
+      using (var repo = MainRepository.WithReadAndWrite())
+      {
+        var member = await repo.Unit.GetByMemberIdAsync(this.AuthData.CharacterId);
+        var oldUnit = member.Unit.Data;
+        if (member.Unit.HasData && member.Member.HasData)
+        {
+          var unitMember = member.Member.Data;
+          if (unitMember.Post != UnitMemberPostType.Leader)
+          {
+            ErrorCode.NotPermissionError.Throw();
+          }
+          if (oldUnit.Id != id)
+          {
+            ErrorCode.NotPermissionError.Throw();
+          }
+        }
+        else
+        {
+          ErrorCode.UnitNotFoundError.Throw();
+        }
+
+        var members = await repo.Unit.GetMembersAsync(id);
+        var targetMember = members.FirstOrDefault(m => m.CharacterId == newLeader.CharacterId);
+        if (targetMember == null)
+        {
+          ErrorCode.InvalidOperationError.Throw();
+        }
+        if (targetMember.Post == UnitMemberPostType.Leader)
+        {
+          ErrorCode.MeaninglessOperationError.Throw();
+        }
+        var targetChara = await repo.Character.GetByIdAsync(targetMember.CharacterId);
+        if (!targetChara.HasData)
+        {
+          ErrorCode.CharacterNotFoundError.Throw();
+        }
+        if (targetChara.Data.AiType != CharacterAiType.Human)
+        {
+          ErrorCode.InvalidOperationError.Throw();
+        }
+        targetMember.Post = UnitMemberPostType.Leader;
+        member.Member.Data.Post = UnitMemberPostType.Normal;
+
+        await repo.SaveChangesAsync();
+      }
+    }
+
+    [AuthenticationFilter]
+    [HttpDelete("unit/{id}")]
+    public async Task RemoveUnitAsync(
+      [FromRoute] uint id)
+    {
+      using (var repo = MainRepository.WithReadAndWrite())
+      {
+        var member = await repo.Unit.GetByMemberIdAsync(this.AuthData.CharacterId);
+        if (member.Unit.HasData && member.Member.HasData)
+        {
+          var unitMember = member.Member.Data;
+          var unit = member.Unit.Data;
+          if (unitMember.Post != UnitMemberPostType.Leader)
+          {
+            ErrorCode.NotPermissionError.Throw();
+          }
+          if (unit.Id != id)
+          {
+            ErrorCode.NotPermissionError.Throw();
+          }
+        }
+        else
+        {
+          ErrorCode.UnitNotFoundError.Throw();
+        }
+
+        await UnitService.RemoveAsync(repo, id);
+        await repo.SaveChangesAsync();
+      }
+    }
+  }
+}
