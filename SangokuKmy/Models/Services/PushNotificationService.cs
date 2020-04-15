ca24405893f32e9d109ed6cb35b06ad654e56b60
雪あすか <tt@kmycode.net>
2020-04-16 07:45:36 +0900

    初期イベントのプッシュ通知を追加

--- SangokuKmy/Models/Services/PushNotificationService.cs
+++ SangokuKmy/Models/Services/PushNotificationService.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Server.Kestrel;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Linq;
 using PushSharp.Apple;
@@ -10,27 +13,44 @@ namespace SangokuKmy.Models.Services
 {
   public static class PushNotificationService
   {
-    public static async Task TestSend(MainRepository repo, SystemData d, ILogger logger)
+    public static ILogger Logger { get; set; }
+
+    public static Task SendAllAsync(MainRepository repo, string title, string message)
+      => SendAsync(repo, title, message, k => true);
+
+    public static Task SendCharacterAsync(MainRepository repo, string title, string message, uint charaId)
+      => SendAsync(repo, title, message, k => k.CharacterId == charaId);
+
+    public static Task SendCharactersAsync(MainRepository repo, string title, string message, IEnumerable<uint> charaIds)
+      => SendAsync(repo, title, message, k => charaIds.Contains(k.CharacterId));
+
+    public static async Task SendCountryAsync(MainRepository repo, string title, string message, uint countryId)
+    {
+      var charas = await repo.Country.GetCharactersAsync(countryId);
+      await SendCharactersAsync(repo, title, message, charas.Select(c => c.Id));
+    }
+
+    private static async Task SendAsync(MainRepository repo, string title, string message, Predicate<PushNotificationKey> predicate)
     {
       try
       {
         var push = new ApnsServiceBroker(new ApnsConfiguration(
-          ApnsConfiguration.ApnsServerEnvironment.Sandbox,
-          "/home/sangokukmy/push_notification_development.p12",
+          ApnsConfiguration.ApnsServerEnvironment.Production,
+          "/home/sangokukmy/push_notification_product.p12",
           "test"));
         push.OnNotificationFailed += (sender, e) =>
         {
-
+          Logger.LogError(e, "プッシュ通知送信時にエラーが発生しました");
         };
         push.Start();
 
         var keys = await repo.PushNotificationKey.GetAllAsync();
-        foreach (var key in keys)
+        foreach (var key in keys.Where(k => k.Platform == PushNotificationPlatform.iOS && predicate(k)))
         {
           push.QueueNotification(new ApnsNotification
           {
             DeviceToken = key.Key,
-            Payload = JObject.Parse(@"{""aps"":{""alert"":{""title"":""リセット"",""body"":""ゲームはリセットされ、第 " + d.Period + @" が始まりました。新規登録しましょう。""},""badge"":7}}"),
+            Payload = JObject.Parse(@"{""aps"":{""alert"":{""title"":""" + title + @""",""body"":""" + message + @"""},""badge"":1}}"),
           });
         }
 
@@ -38,7 +58,7 @@ namespace SangokuKmy.Models.Services
       }
       catch (Exception ex)
       {
-        logger.LogError(ex, "プッシュ通知で例外が発生しました");
+        Logger?.LogError(ex, "プッシュ通知で例外が発生しました");
       }
     }
   }
