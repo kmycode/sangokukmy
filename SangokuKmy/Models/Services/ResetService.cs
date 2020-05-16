caa8d05e9918fb4b406669dc7f7065b355df8b83
雪あすか <tt@kmycode.net>
2020-05-16 23:15:38 +0900

    来期の都市数調整

--- SangokuKmy/Models/Services/ResetService.cs
+++ SangokuKmy/Models/Services/ResetService.cs
@@ -230,7 +230,7 @@ namespace SangokuKmy.Models.Services
 
     private static async Task ResetTownsAndSaveAsync(MainRepository repo)
     {
-      var initialTowns = MapService.CreateMap(RandomService.Next(10, 13));
+      var initialTowns = MapService.CreateMap(RandomService.Next(8, 11));
       var towns = new List<Town>();
       foreach (var itown in initialTowns)
       {
