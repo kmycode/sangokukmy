71888635463e6b00d344e80bc2584bce40912530
KMY <tt@kmycode.net>
2020-08-30 14:25:10 +0900

    戦争中・同盟中の国の購入コストを調整

--- SangokuKmy/Models/Services/TownService.cs
+++ SangokuKmy/Models/Services/TownService.cs
@@ -16,7 +16,21 @@ namespace SangokuKmy.Models.Services
       var townTypeCost = (countryOptional.HasData && countryOptional.Data.CapitalTownId == town.Id) ? 50000 :
         town.Type == TownType.Large ? 25000 : 15000;
 
-      return (int)(townTypeCost * Math.Pow(1.03f, townCount) * Math.Pow(1.12f, characterCount) + town.TakeoverDefensePoint);
+      var cost = townTypeCost * Math.Pow(1.03f, townCount) * Math.Pow(1.12f, characterCount) + town.TakeoverDefensePoint;
+
+      var war = await repo.CountryDiplomacies.GetCountryWarAsync(country.Id, town.CountryId);
+      if (war.HasData && (war.Data.Status == CountryWarStatus.Available || war.Data.Status == CountryWarStatus.InReady || war.Data.Status == CountryWarStatus.StopRequesting))
+      {
+        cost *= 3;
+      }
+
+      var alliance = await repo.CountryDiplomacies.GetCountryAllianceAsync(country.Id, town.CountryId);
+      if (alliance.HasData && (alliance.Data.Status == CountryAllianceStatus.Available || alliance.Data.Status == CountryAllianceStatus.InBreaking))
+      {
+        cost *= 2;
+      }
+
+      return (int)cost;
     }
   }
 }
