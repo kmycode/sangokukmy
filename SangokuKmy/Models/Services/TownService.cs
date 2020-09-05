3d2359fd6f0f71d0fbee5d7db8d839c1fcfe3148
KMY <tt@kmycode.net>
2020-09-05 21:51:29 +0900

    都市購入コストを調整

--- SangokuKmy/Models/Services/TownService.cs
+++ SangokuKmy/Models/Services/TownService.cs
@@ -15,7 +15,7 @@ namespace SangokuKmy.Models.Services
       var townCount = await repo.Town.CountByCountryIdAsync(country.Id);
       var characterCount = await repo.Country.CountCharactersAsync(country.Id, true);
       var townTypeCost = (countryOptional.HasData && countryOptional.Data.CapitalTownId == town.Id) ? 12000 :
-        town.Type == TownType.Large ? 8000 : 6000;
+        town.Type == TownType.Large ? 10000 : 8000;
 
       var cost = townTypeCost * (Math.Pow(1.06f, townCount) + Math.Pow(1.11f, characterCount)) + town.TakeoverDefensePoint;
 
