47d6bba45aa8daaff4cf4101d0c694a3a7337327
KMY <tt@kmycode.net>
2020-10-14 21:13:10 +0900

    宗教戦争の修正３

--- SangokuKmy/Models/Commands/BattleCommand.cs
+++ SangokuKmy/Models/Commands/BattleCommand.cs
@@ -567,21 +567,21 @@ namespace SangokuKmy.Models.Commands
           var targetDamage = targetFirstNumber - targetCharacter.SoldierNumber;
           if (religion == ReligionType.Confucianism)
           {
-            targetFirstNumber -= targetTown.Confucianism;
+            // targetFirstNumber -= targetTown.Confucianism;
             var rate = Math.Max(0.0f, 1.0f - (float)targetDamage / targetFirstNumber);
             targetTown.Taoism = (int)(targetTown.Taoism * rate);
             targetTown.Buddhism = (int)(targetTown.Buddhism * rate);
           }
           if (religion == ReligionType.Taoism)
           {
-            targetFirstNumber -= targetTown.Taoism;
+            // targetFirstNumber -= targetTown.Taoism;
             var rate = Math.Max(0.0f, 1.0f - (float)targetDamage / targetFirstNumber);
             targetTown.Confucianism = (int)(targetTown.Confucianism * rate);
             targetTown.Buddhism = (int)(targetTown.Buddhism * rate);
           }
           if (religion == ReligionType.Buddhism)
           {
-            targetFirstNumber -= targetTown.Buddhism;
+            // targetFirstNumber -= targetTown.Buddhism;
             var rate = Math.Max(0.0f, 1.0f - (float)targetDamage / targetFirstNumber);
             targetTown.Confucianism = (int)(targetTown.Confucianism * rate);
             targetTown.Taoism = (int)(targetTown.Taoism * rate);
